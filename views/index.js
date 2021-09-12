c205243aee391c8afcdbd50dc0a85aa2f7b717d2
Nina <morville5audrey@gmail.com>
2021-09-12 19:56:54 +0200

    cleaning files

--- views/index.js
+++ views/index.js
@@ -1,143 +1,6 @@
 
-/*fetch('http://localhost:3000/api/teddies')
-    .then((resp) => 
-        console.log(resp.json())
-    )
-*/
-
-
-
-
-/*
-const imageText = document.getElementById("image-text")
-const shop = document.getElementById("shop")
-
-
-fetch('http://localhost:3000/api/teddies')
-    .then(resp =>
-        resp.json()
-    )
-    .then(data => {
-        console.log(data);
-        let liste = `<ul>`;
-            for (let teddy of data){
-            liste += `<img src="${teddy.imageUrl}">`;
-            liste += `<li>${teddy.name}</li>`;
-            liste +=  `<li>${teddy.price}</li>`;
-
-            }
-        liste += `</ul>`;
-        document.getElementById("shop").innerHTML = liste;  
-        }
-    ) 
-    .catch((e) => console.log(e))
-
-    */
-
-
-
-
-/*
-const imageText = document.getElementById("image-text")
-const shop = document.getElementById("shop")
-
-
-fetch('http://localhost:3000/api/teddies')
-    .then(resp =>
-        resp.json()
-    )
-    .then(data => {
-        console.log(data);
-        let liste = `<a href="produit.js">`;
-            for (let teddy of data){
-            liste += `<div>${teddy.name}</div>`;
-            liste +=  `<div>${teddy.price.add}</div>`;
-            liste += `<img src="${teddy.imageUrl}"width=20%>`;
-            }
-        liste += `</a>`;
-        document.getElementById("shop").innerHTML = liste; 
-        document.body.style.color = "white";
-        }
-    ) 
-    .catch((e) => console.log(e))
-
-    */
-
-
-    
-  
-
-
-
-
-
-
-
-
-
-/*
-
-fetch('http://localhost:3000/api/teddies')
-    .then(resp =>
-        resp.json()
-    )
-    .then(data => {        
-        let liste = `<a href="../produit.html">`;
-
-            for (let teddy of data){
-            liste += `<img src="${teddy.imageUrl}"width=100%>`;
-            liste += `<p> ${teddy.name}</p> `;
-            liste += `<p> ${teddy.price}</p> `; 
-
-            liste += `</a>`;
-            }  
-        const shop = document.querySelector("#shop")
-        shop.innerHTML = liste
-        }  
-    ) 
-    .catch((e)=> console.log(e))
-*/
-
-
-//--------
-//fonction
-//--------
-
-/*
-const url = ('http://localhost:3000/api/teddies')
-//mettre l'api des teddies dans la constante url
-const bigBox = document.getElementById("shop")
-//faire la boucle a l'interieur de la bigBox
-const smallCard = document.getElementById("teddy")
-//ajouter les images dans ces divs puis injecter les noms d'api puis append les smallCards dans la bigBox
-const imgCard = document.getElementById("img-card")
-
-const nomination = document.getElementById("nomination")
-const price = document.getElementById("price")
-
-fetch(url)
-    .then(resp =>
-        resp.json()
-    )
-    .then(data => {        
-
-        for (let teddy of data){ 
-            smallCard.classList.add(".card")
-            imgCard.classList.add("pic-style")
-            imgCard.innerHTML = `<img src = "${teddy.imageUrl}">`;
-            price.innerHTML = teddy.price + '$'
-            nomination.innerHTML = teddy.name
-            smallCard.append(nomination)
-            smallCard.append(price)
-            bigBox.append(smallCard) 
-        }  
-    }  
-        
-    ) 
-    .catch((e)=> console.log(e)) 
-
-
-    */
+const box = document.getElementById("teddy")
+const cardTeddy = document.getElementById("card-teddy")
 
 
 
@@ -153,7 +16,6 @@ function getProducts(){
     )
     .catch((e)=> console.log(e));
 }
-const box = document.getElementById("teddy")
 
 function showProducts(products){
     for (let teddy of products){
@@ -169,12 +31,7 @@ function showProducts(products){
         </a>`;
     }
 }
-
 getProducts();
-
-
-
-
 
 
 
@@ -193,7 +50,6 @@ function getOneProduct(){
 
 
 
-const cardTeddy = document.getElementById("card-teddy")
 
 function showOneProduct(product){
     cardTeddy.innerHTML +=
@@ -206,25 +62,11 @@ function showOneProduct(product){
             <p class ="info-price">${teddy.price}$</p>
         </div>
     </div>`;
-
 }
 
 
-box.addEventListener("click", e =>{
+document.box.addEventListener("click", e =>{
     e.preventDefault();
     showOneProduct(teddy);
 } 
 )
-
-
-/*
-function chooseTeddy() {
-    box.addEventListener("click", e =>{
-        e.preventDefault();
-        let choosen = $this;
-        showProducts(choosen);
-    } 
-    )
-}
-chooseTeddy();
-*/
--- views/produit.js
+++ views/produit.js
@@ -1,50 +1,7 @@
 
-
-/*
-function main() {
-
-    //trouver une fonction pour afficher ma liste d'article 
-    console.log("je suis heureuse");
-
-    fetch('http://localhost:3000/api/teddies')
-    .then((resp) => console.log(resp.json()));
-
-}
-
-main();
-*/
-
-
-
-/*
-const a = document.querySelector("a")
-a.addEventListener(click =>{
-    
-    fetch('http://localhost:3000/api/teddies')
-        .then(resp =>
-        resp.json()
-        )
-        .then(data => { 
-             liste += `<img src="${teddy.imageUrl}"width=100%>`
-            liste += `<p> ${teddy.name}</p> `
-            liste += `<p> ${teddy.price}</p> `
-            liste += `</a>`
-        }
-        
-        )
-        .catch((e)=> console.log(e))
-        const shop = document.querySelector("#shop")
-        shop.innerHTML = liste
-        
-}
-    )
-    
-*/
-
-
-
-
 const cardTeddy = document.getElementById("card-teddy")
+
+
 
 function getOneProduct(){
     console.log("function get one product");
