9bae9f2555fac8f724fe24d3e84c46201e8be043
Nina <morville5audrey@gmail.com>
2021-09-16 17:49:21 +0200

    cart local storage v2

--- views/panier.js
+++ views/panier.js
@@ -1,3 +1,4 @@
+//declaration des variables globales
 const boxCart = document.getElementById('box-cart')
 const ajout = document.getElementsByClassName('ajout')
 
@@ -5,33 +6,55 @@ const ajout = document.getElementsByClassName('ajout')
 //gestion du local storage pour recuperer l'id posé dans le panier
 
 //creation du addEventListener
-ajout.addEventListener ("click", e =>{
-    e.preventDefault()
+//ajout.addEventListener("click", () =>{
+    //e.preventDefault()
 
 //verifier si notre clé existe ou pas dans le local storage
 if (!localStorage.getItem("produit")){
     createStorage()
+    recupStorage()
     }else {
         recupStorage()
     }
 
 //pour sauvegarder les données dans le local storage
-createStorage =>{
+function createStorage(){
 //convertir l'objet javascript en objet json
-    const newStorage = localStorage.setItem(/*clé*/"produit", /*objet*/ JSON.stringify("teddyId"))
-    console.log(newStorage)
+    localStorage.setItem(/*clé*/"produit", /*objet*/ JSON.stringify("${teddyId}"))
+    console.log("crée")
 } 
 
 //pour recuperer les données sauvegardées
-recupStorage => {
-    //const getStorage = localStorage.getItem("produit")
-    const getStorage = localStorage.getItem(JSON.parse("produit"))
-    console.log(getStorage)
+function recupStorage(){
+//lire l'objet json en clair
+    //localStorage.getItem((JSON.parse("produit"))
+    localStorage.getItem(("produit"))
+    console.log("recuperé")
+
+    //mise en page avec les données recuperées
+    const elementPanier = document.createElement('div')
+    elementPanier.classList.add('cart-item')
+
+
+
+    
+    for (const img of produit.imageUrl) {
+        console.log(imageUrl)
+        const img = document.createElement('img')
+        img.setAttribute('src', '${produit.imageUrl}')
+
+    }
+
+
+
+    boxCart.appendChild(elementPanier)
+    elementPanier.appendChild(img)
 }
+
 
 //json.parse c'est pour convertir les donnees au format json qui sont dans le local storage en objet javascript
 
 //convertir le json en object javascript pour le lire
 
 //fin du addEventListener
-})
+//})
